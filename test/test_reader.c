94a502f6d38b16b3f29bf2e8e74a289a12ee929d
emasaka <emasaka@gmail.com>
2024-01-05 22:49:58 +0900

    unit test for reader.c

---
+++ test/test_reader.c
@@ -0,0 +1,208 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include "../src/reader.c"
+
+/* from writer.c */
+extern int writer(Lisp_Object, char *);
+
+#define TMP_BUFFSIZE 128
+
+int init_for_readertest(void) {
+    init_mempool();
+    init_symbol();
+    return 0;
+}
+
+/*
+  numbers
+ */
+
+void test_reader_positive_int(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("31");
+    CU_ASSERT(obj.type == Lisp_Int);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "31") == 0);
+}
+
+void test_reader_negative_int(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("-31");
+    CU_ASSERT(obj.type == Lisp_Int);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "-31") == 0);
+}
+
+void test_reader_char(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("?a");
+    CU_ASSERT(obj.type == Lisp_Int);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "97") == 0);
+}
+
+void testsuite_reader_number(void) {
+    CU_pSuite suite = CU_add_suite("reader_number", init_for_readertest, NULL);
+    CU_add_test(suite, "reader_positive_int", test_reader_positive_int);
+    CU_add_test(suite, "reader_negative_int", test_reader_negative_int);
+    CU_add_test(suite, "reader_char", test_reader_char);
+}
+
+/*
+  symbol
+ */
+
+void test_reader_symbol(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("foo");
+    CU_ASSERT(obj.type == Lisp_Symbol);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "foo") == 0);
+}
+
+void testsuite_reader_symbol(void) {
+    CU_pSuite suite = CU_add_suite("reader_symbol", init_for_readertest, NULL);
+    CU_add_test(suite, "reader_symbol", test_reader_symbol);
+}
+
+/*
+  nil
+ */
+
+void test_reader_nil(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("nil");
+    CU_ASSERT(obj.type == Lisp_Nil);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "nil") == 0);
+}
+
+void testsuite_reader_nil(void) {
+    CU_pSuite suite = CU_add_suite("reader_nil", init_for_readertest, NULL);
+    CU_add_test(suite, "reader_nil", test_reader_nil);
+}
+
+/*
+  string
+ */
+
+void test_reader_string_visible(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("\"Hello, World!\"");
+    CU_ASSERT(obj.type == Lisp_String);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "\"Hello, World!\"") == 0);
+}
+
+void test_reader_string_escaped_octet1(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("\"I\\057O\"");
+    CU_ASSERT(obj.type == Lisp_String);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "\"I/O\"") == 0);
+}
+
+void test_reader_string_escaped_octet2(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("\"\\033\"");
+    CU_ASSERT(obj.type == Lisp_String);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "\"\\033\"") == 0);
+}
+
+void test_reader_string_escaped_visible(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("\"\\W\"");
+    CU_ASSERT(obj.type == Lisp_String);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "\"W\"") == 0);
+}
+
+void testsuite_reader_string(void) {
+    CU_pSuite suite = CU_add_suite("reader_string", init_for_readertest, NULL);
+    CU_add_test(suite, "reader_string_visible", test_reader_string_visible);
+    CU_add_test(suite, "reader_string_escaped_octet1", test_reader_string_escaped_octet1);
+    CU_add_test(suite, "reader_string_escaped_octet2", test_reader_string_escaped_octet2);
+    CU_add_test(suite, "reader_string_escaped_visible", test_reader_string_escaped_visible);
+}
+
+/*
+  list
+ */
+
+void test_reader_list_empty(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("()");
+    CU_ASSERT(obj.type == Lisp_CList);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "()") == 0);
+}
+
+void test_reader_list_one(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("(foo)");
+    CU_ASSERT(obj.type == Lisp_CList);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "(foo)") == 0);
+}
+
+void test_reader_list_two(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("(foo bar)");
+    CU_ASSERT(obj.type == Lisp_CList);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "(foo bar)") == 0);
+}
+
+void test_reader_list_quoted(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("'foo");
+    CU_ASSERT(obj.type == Lisp_CList);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "(quote foo)") == 0);
+}
+
+void test_reader_list_nested1(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("((foo))");
+    CU_ASSERT(obj.type == Lisp_CList);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "((foo))") == 0);
+}
+
+void test_reader_list_nested2(void) {
+    char tmp_buf[TMP_BUFFSIZE];
+    Lisp_Object obj = reader("(lambda (a b) (concat a b))");
+    CU_ASSERT(obj.type == Lisp_CList);
+    CU_ASSERT(writer(obj, tmp_buf) == 0);
+    CU_ASSERT(strcmp(tmp_buf, "(lambda (a b) (concat a b))") == 0);
+}
+
+void testsuite_reader_list(void) {
+    CU_pSuite suite = CU_add_suite("reader_list", init_for_readertest, NULL);
+    CU_add_test(suite, "reader_list_empty", test_reader_list_empty);
+    CU_add_test(suite, "reader_list_one", test_reader_list_one);
+    CU_add_test(suite, "reader_list_two", test_reader_list_two);
+    CU_add_test(suite, "reader_list_quoted", test_reader_list_quoted);
+    CU_add_test(suite, "reader_list_nested1", test_reader_list_nested1);
+    CU_add_test(suite, "reader_list_nested2", test_reader_list_nested2);
+}
+
+/*
+  Main
+ */
+
+int main(void) {
+    CU_initialize_registry();
+
+    testsuite_reader_number();
+    testsuite_reader_symbol();
+    testsuite_reader_nil();
+    testsuite_reader_string();
+    testsuite_reader_list();
+
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return 0;
+}
