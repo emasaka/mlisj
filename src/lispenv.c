0b6d5942e217558dc2bfce5155b0ec8cb367447e
emasaka <emasaka@gmail.com>
2024-02-17 13:10:52 +0900

    add a bundle of initialization functions

---
+++ src/lispenv.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "lispenv.h"
+
+void end_lispenv(lispenv_t *env) {
+    if (env->func_pool != NULL) { end_func_table(env->func_pool); }
+    if (env->variable_pool != NULL) { end_variables(env->variable_pool); }
+    if (env->symbol_pool != NULL) { end_symbol(env->symbol_pool); }
+    if (env->mempool != NULL) { end_mempool(env->mempool); }
+    free(env);
+}
+
+lispenv_t *init_lispenv(void) {
+    lispenv_t *env = malloc(sizeof(lispenv_t));
+    if (env == NULL) {
+        return NULL;
+    }
+
+    env->mempool = NULL;
+    env->symbol_pool = NULL;
+    env->variable_pool = NULL;
+    env->func_pool = NULL;
+
+    mempool_t *mempool = init_mempool();
+    if (mempool == NULL) {
+        end_lispenv(env);
+        return NULL;
+    }
+    env->mempool = mempool;
+
+    symbol_pool_t *symbol_pool = init_symbol(mempool);
+    if (symbol_pool == NULL) {
+        end_lispenv(env);
+        return NULL;
+    }
+    env->symbol_pool = symbol_pool;
+
+    variable_pool_t *variable_pool = init_variables(symbol_pool);
+    if (variable_pool == NULL) {
+        end_lispenv(env);
+        return NULL;
+    }
+    env->variable_pool = variable_pool;
+
+    func_pool_t *func_pool = init_func_table(symbol_pool);
+    if (func_pool == NULL) {
+        end_lispenv(env);
+        return NULL;
+    }
+    env->func_pool = func_pool;
+
+    return env;
+}
---
+++ src/lispenv.h
@@ -0,0 +1,20 @@
+#ifndef _LISPENV_H
+#define _LISPENV_H
+
+#include "lispobject.h"
+#include "mempool.h"
+#include "symbol.h"
+#include "variable.h"
+#include "functable.h"
+
+typedef struct {
+    mempool_t *mempool;
+    symbol_pool_t *symbol_pool;
+    variable_pool_t *variable_pool;
+    func_pool_t *func_pool;
+} lispenv_t;
+
+extern lispenv_t *init_lispenv(void);
+extern void end_lispenv(lispenv_t *);
+
+#endif /* _LISPENV_H */
