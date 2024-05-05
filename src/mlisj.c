95fab5a99602d886cdc3d9f1fe0f7381d16288c1
emasaka <emasaka@gmail.com>
2024-05-05 11:45:41 +0900

    entry function mlisj_eval()

---
+++ src/mlisj.c
@@ -0,0 +1,64 @@
+#include <string.h>
+#include "mlisj.h"
+#include "lispobject.h"
+#include "lispenv.h"
+#include "reader.h"
+#include "eval.h"
+#include "func-simple.h"
+#include "func-skk-gadgets.h"
+
+static int mlisj_init(char **skk_num_list, const char *skk_henkan_key, lispenv_t * env) {
+    if (register_func_simple(env) != 0) {
+        end_lispenv(env);
+        return -1;
+    }
+    if (register_func_skk_gadgets(env) != 0) {
+        end_lispenv(env);
+        return -1;
+    }
+    register_skk_num_list(env, skk_num_list);
+    register_skk_henkan_key(env, skk_henkan_key);
+
+    return 0;
+}
+
+int mlisj_eval(const char *src, char *dest, size_t size, char **skk_num_list, const char *skk_henkan_key) {
+    /* initialize*/
+    lispenv_t *env = init_lispenv();
+    if (env == NULL) { return MLISJ_ERROR_RUNTIME; }
+
+    int exitcode = 0;
+    if (mlisj_init(skk_num_list, skk_henkan_key, env) != 0) {
+        exitcode = MLISJ_ERROR_RUNTIME;
+        goto exit;
+    }
+
+    /* read */
+    Lisp_Object lisp_expr = reader(src, env);
+    if (GET_TYPE(lisp_expr) == Internal_Error) {
+        exitcode = (GET_ERROR_TYPE(lisp_expr) == Reader_Error) ?
+                MLISJ_ERROR_EVALUATION :
+                MLISJ_ERROR_RUNTIME;
+        goto exit;
+    }
+
+    /* evaluate */
+    Lisp_Object result = eval_expr(lisp_expr, env);
+    switch (GET_TYPE(result)) {
+    case Lisp_String:
+        strncpy(dest, GET_SVAL(result), size);
+        dest[(size > 0) ? (size - 1) : 0] = 0;
+        break;
+    case Internal_Error:
+        exitcode = (GET_ERROR_TYPE(lisp_expr) == Evaluation_Error) ?
+                MLISJ_ERROR_EVALUATION : MLISJ_ERROR_RUNTIME;
+        break;
+    default:
+        exitcode = MLISJ_ERROR_EVALUATION;
+        break;
+    }
+
+exit:
+    end_lispenv(env);
+    return exitcode;
+}
---
+++ src/mlisj.h
@@ -0,0 +1,9 @@
+#ifndef _MLISJ_H
+#define _MLISJ_H
+
+extern int mlisj_eval(const char *, char *, size_t, char **, const char *);
+
+#define MLISJ_ERROR_EVALUATION 1
+#define MLISJ_ERROR_RUNTIME 2
+
+#endif /* _MLISJ_H */
