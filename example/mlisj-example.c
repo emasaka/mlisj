a11169fe373dca0b384e2fd47ac7d274911be7ea
emasaka <emasaka@gmail.com>
2024-10-01 19:52:33 +0900

    example of skk-gengo-to-ad

--- example/mlisj-example.c
+++ example/mlisj-example.c
@@ -11,11 +11,11 @@
 
 #define BUFSIZE 256
 
-void call_mlisj(char *str, char **skk_num_list) {
+void call_mlisj(char *str, char **skk_num_list, char *henkan_key) {
     char buffer[BUFSIZE];
 
     puts(str);
-    int ret = mlisj_eval(str, buffer, BUFSIZE, skk_num_list, NULL);
+    int ret = mlisj_eval(str, buffer, BUFSIZE, skk_num_list, henkan_key);
     if (ret == 0) {
         printf("-> %s\n", buffer);
     } else if (ret == MLISJ_ERROR_EVALUATION) {
@@ -26,17 +26,20 @@ void call_mlisj(char *str, char **skk_num_list) {
 }
 
 int main() {
-    call_mlisj("(笑)", NULL);
-    call_mlisj("(concat \"I\057O\")", NULL);
-    call_mlisj("(symbol-value 'user-full-name)", NULL);
-    call_mlisj("(make-string (- (window-width) 5) ?-)", NULL);
-    call_mlisj("(current-time-string)", NULL);
-    call_mlisj("(substring (current-time-string) 11 16)", NULL);
-    call_mlisj("(skk-current-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)))", NULL);
-    call_mlisj("(skk-relative-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)) nil nil :dd -1)", NULL);
+    call_mlisj("(笑)", NULL, NULL);
+    call_mlisj("(concat \"I\057O\")", NULL, NULL);
+    call_mlisj("(symbol-value 'user-full-name)", NULL, NULL);
+    call_mlisj("(make-string (- (window-width) 5) ?-)", NULL, NULL);
+    call_mlisj("(current-time-string)", NULL, NULL);
+    call_mlisj("(substring (current-time-string) 11 16)", NULL, NULL);
+    call_mlisj("(skk-current-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)))", NULL, NULL);
+    call_mlisj("(skk-relative-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)) nil nil :dd -1)", NULL, NULL);
 
-    char *num_list[] = { "3", "5", NULL };
-    call_mlisj("(skk-times)", num_list);
+    char *num_list1[] = { "3", "5", NULL };
+    call_mlisj("(skk-times)", num_list1, NULL);
+
+    char *num_list2[] = { "60", NULL };
+    call_mlisj("(skk-gengo-to-ad \"\" \"年\")", num_list2, "しょうわ60ねん");
 
     return 0;
 }
--- example/mlisj-example.py
+++ example/mlisj-example.py
@@ -21,12 +21,14 @@ def make_c_strary(strlist):
     ary[-1] = None
     return ary
 
-def call_mlisj(str, skk_num_list):
+def call_mlisj(str, skk_num_list=None, henkan_key=None):
     print(str)
     c_str = str.encode()
     buffer = ctypes.create_string_buffer(BUFSIZE)
     c_skk_num_list = make_c_strary(skk_num_list)
-    ret = mlisj_eval(c_str, buffer, BUFSIZE, c_skk_num_list, None)
+    if henkan_key is not None:
+        henkan_key = henkan_key.encode()
+    ret = mlisj_eval(c_str, buffer, BUFSIZE, c_skk_num_list, henkan_key)
     if ret == 0:
         outstr = buffer.value.decode()
         print("-> %s" % outstr)
@@ -36,12 +38,13 @@ def call_mlisj(str, skk_num_list):
         print("-> error!")
 
 ## main
-call_mlisj("(笑)", None)
-call_mlisj("(concat \"I\057O\")", None)
-call_mlisj("(symbol-value 'user-full-name)", None)
-call_mlisj("(make-string (- (window-width) 5) ?-)", None)
-call_mlisj("(current-time-string)", None)
-call_mlisj("(substring (current-time-string) 11 16)", None)
-call_mlisj("(skk-current-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)))", None)
-call_mlisj("(skk-relative-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)) nil nil :dd -1)", None)
-call_mlisj("(skk-times)", ["3", "5"])
+call_mlisj("(笑)")
+call_mlisj("(concat \"I\057O\")")
+call_mlisj("(symbol-value 'user-full-name)")
+call_mlisj("(make-string (- (window-width) 5) ?-)")
+call_mlisj("(current-time-string)")
+call_mlisj("(substring (current-time-string) 11 16)")
+call_mlisj("(skk-current-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)))")
+call_mlisj("(skk-relative-date (lambda (date-information format gengo and-time) (skk-default-current-date date-information \"%s-%s-%s(%s)\" 0 nil 0 0 nil)) nil nil :dd -1)")
+call_mlisj("(skk-times)", ["3", "5"]);
+call_mlisj("(skk-gengo-to-ad \"\" \"年\")", ["60"], "しょうわ60ねん")
